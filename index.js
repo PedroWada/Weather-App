bea84403e6d61723c65cb1f6e936304bdf081d77
pedro <pedrowada2004@gmail.com>
2024-04-19 13:56:19 +1000

    Changing the folder
    Changing the folder and hiding the .env

---
+++ index.js
@@ -0,0 +1,20 @@
+const express = require('express')
+const cors = require('cors')
+const axios = require('axios')
+require('dotenv').config()
+
+const app = express()
+app.use(cors())
+
+app.get('/weather/:location', (req, res) => {
+    const location = req.params.location
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=${process.env.REACT_APP_RAPID_API_KEY}&units=metric`
+    axios.get(url).then((resp) => {
+        res.json(resp.data)
+      }).catch((err)=> {console.log(err)})
+})
+
+port = 8000 || 3000
+app.listen(port,()=> {
+    console.log('Server running on port ' + port)
+})
